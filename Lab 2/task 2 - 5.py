27b6ec179f3d72a61709b8b793dea33f78db3757
alexgaborak <alexgaborak@phystech.edu>
2022-05-28 17:34:20 +0300

    Test refactor

--- Lab 2/task 2 - 5.py
+++ Lab 2/task 2 - 5.py
@@ -1,15 +1,15 @@
-import turtle as t
+import turtle
 
 def square(side):
     for i in range(4):
-        t.forward(side)
-        t.left(90)
+        turtle.forward(side)
+        turtle.left(90)
 
 step = 10
 for i in range(step, 11 * step, step):
     square(i)
-    t.right(135)
-    t.penup()
-    t.forward(step / (2 ** 0.5))
-    t.left(135)
-    t.pendown()
+    turtle.right(135)
+    turtle.penup()
+    turtle.forward(step / (2 ** 0.5))
+    turtle.left(135)
+    turtle.pendown()
