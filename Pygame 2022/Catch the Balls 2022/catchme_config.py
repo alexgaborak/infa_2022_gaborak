21b0d6bd658f9c4cc041ae524e5ca35c345fcd6e
alexgaborak <alexgaborak@phystech.edu>
2022-12-20 20:48:59 +0300

    Done!

--- Pygame 2022/Catch the Balls 2022/catchme_config.py
+++ Pygame 2022/Catch the Balls 2022/catchme_config.py
@@ -1,5 +1,5 @@
 # Параметры экрана и изображений
-MAX_X, MAX_Y = 800, 800
+MAX_X, MAX_Y = 1200, 800
 FPS = 60
 
 # Цвета
@@ -13,13 +13,13 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 # Параметры шариков
-MIN_RADIUS = 50
-MAX_RADIUS = 100
+MIN_RADIUS = 20
+MAX_RADIUS = 30
 MIN_X_OF_CENTRE = MAX_RADIUS
 MAX_X_OF_CENTRE = MAX_X - MAX_RADIUS
 MIN_Y_OF_CENTRE = MAX_RADIUS
 MAX_Y_OF_CENTRE = MAX_Y - MAX_RADIUS
-MAX_SPEED = 1
+MAX_SPEED = 25
 
 x_0 = x_1 = x_2 = 0  # чтобы не вылетала ошибка при клике до появления первого шарика
 y_0 = y_1 = y_2 = 0
--- Pygame 2022/Catch the Balls 2022/catchme_main.py
+++ Pygame 2022/Catch the Balls 2022/catchme_main.py
@@ -4,7 +4,7 @@ from random import randint
 from catchme_config import *
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((MAX_X, MAX_Y))
 
 # Шрифт
 FONT = pygame.font.SysFont('couriernew', 30, bold=True)
@@ -24,7 +24,7 @@ def new_ball_0():
     global x_0, y_0, r_0, vx0, vy0, color_0
     x_0 = randint(MIN_X_OF_CENTRE, MAX_X_OF_CENTRE)
     y_0 = randint(MIN_Y_OF_CENTRE, MAX_Y_OF_CENTRE)
-    r_0 = randint(MIN_RADIUS, MAX_RADIUS)
+    r_0 = 4 * randint(MIN_RADIUS, 2*MAX_RADIUS)
     vx0 = random_speed()
     vy0 = random_speed()
     color_0 = COLORS[randint(0, 5)]
@@ -36,9 +36,9 @@ def move_target_0():
     :return none:
     """
     global x_0, y_0
+    circle(screen, color_0, (x_0, y_0), r_0)
     x_0 += vx0
     y_0 += vy0
-    circle(screen, color_0, (x_0, y_0), r_0)
 
 
 def new_ball_1():  # idk понадобится или нет
@@ -51,7 +51,7 @@ def new_ball_1():  # idk понадобится или нет
     global x_1, y_1, r_1, vx1, vy1, color_1
     x_1 = randint(MIN_X_OF_CENTRE, MAX_X_OF_CENTRE)
     y_1 = randint(MIN_Y_OF_CENTRE, MAX_Y_OF_CENTRE)
-    r_1 = randint(MIN_RADIUS, MAX_RADIUS)
+    r_1 = 2 * randint(MIN_RADIUS, MAX_RADIUS)
     vx1 = random_speed()
     vy1 = random_speed()
     color_1 = COLORS[randint(0, 5)]
@@ -63,14 +63,15 @@ def move_target_1():
     :return:
     """
     global x_1, y_1
+    circle(screen, color_1, (x_1, y_1), r_1)
     x_1 += vx1
     y_1 += vy1
-    circle(screen, color_1, (x_1, y_1), r_1)
 
 
-def new_special_figure():  # пока третий шарик
+def new_special_figure():
     """
-    Рисует "шарики" - круги случайного радиуса r_1
+    Рисует "бублики" - круги случайного радиуса r_2 и дыркой шириной r_2
+    Бублики потому, что пока я это делал, я сильно захотел жрать
     (x_2, y_2) - случайные координаты центра кругов
     (vx2, vy2) - проекции скоростей
     color_2 - случайный цвет из COLORS
@@ -78,7 +79,7 @@ def new_special_figure():  # пока третий шарик
     global x_2, y_2, r_2, vx2, vy2, color_2
     x_2 = randint(MIN_X_OF_CENTRE, MAX_X_OF_CENTRE)
     y_2 = randint(MIN_Y_OF_CENTRE, MAX_Y_OF_CENTRE)
-    r_2 = randint(MIN_RADIUS/10, MAX_RADIUS/4)
+    r_2 = randint(MIN_RADIUS, MAX_RADIUS)
     vx2 = 2 * random_speed()
     vy2 = 2 * random_speed()
     color_2 = COLORS[randint(0, 5)]
@@ -90,9 +91,27 @@ def move_special_target():
     :return:
     """
     global x_2, y_2
+    circle(screen, color_2, (x_2, y_2), r_2, width=int(r_2/2))
     x_2 += vx2
     y_2 += vy2
-    circle(screen, color_2, (x_2, y_2), r_2)
+
+
+def random_speed():
+    """
+    Генерит рандомную проекцию скорости
+    """
+    return randint(-MAX_SPEED, MAX_SPEED)
+
+
+def reflection(velocity_projection):
+    """
+    Случайное отражение от стен. (меняет проекцию скорости на рандомную другого знака)
+    :return int:
+    """
+    if velocity_projection >= 0:
+        return randint(-MAX_SPEED, 0)
+    elif velocity_projection <= 0:
+        return randint(0, MAX_SPEED)
 
 
 def check_click():
@@ -117,24 +136,6 @@ def check_click():
         return 0
 
 
-def random_speed():
-    """
-    Генерит рандомную проекцию скорости
-    """
-    return randint(-MAX_SPEED, MAX_SPEED)
-
-
-def reflection(velocity_projection):
-    """
-    Случайное отражение от стен. (меняет проекцию скорости на рандомную другого знака)
-    :return :
-    """
-    if velocity_projection > 0:
-        return randint(-MAX_SPEED, 0)
-    elif velocity_projection < 0:
-        return randint(0, MAX_SPEED)
-
-
 def current_score():
     """
     Выводит на экран текущий счёт
@@ -150,7 +151,7 @@ def best_players():
     """
     print("Your name:")
     player_name = input()
-    file = open("players.txt", "a")
+    file = open("players.txt", "a",  encoding="utf-8")
     file.write(player_name + " - " + str(score) + "\n")
     file.close()
 
@@ -181,18 +182,38 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             current_score()
-            print(f'', check_click())
             if check_click():
                 initial_set()
+
+    if (x_0 <= r_0 and vx0 < 0) or (x_0 + r_0 >= MAX_X and vx0 > 0):
+        vx0 = reflection(vx0)
+        vy0 = random_speed()
+    if (y_0 <= r_0 and vy0 < 0) or (y_0 + r_0 >= MAX_Y and vy0 > 0):
+        vy0 = reflection(vy0)
+        vx0 = random_speed()
+    if (x_1 <= r_1 and vx1 < 0) or (x_1 + r_1 >= MAX_X and vx1 > 0):
+        vx1 = reflection(vx1)
+        vy1 = random_speed()
+    if (y_1 <= r_1 and vy1 < 0) or (y_1 + r_1 >= MAX_Y and vy1 > 0):
+        vy1 = reflection(vy1)
+        vx1 = random_speed()
+    if (x_2 <= r_2 and vx2 < 0) or (x_2 + r_2 >= MAX_X and vx2 > 0):
+        vx2 = 2 * reflection(vx2)
+        vy2 = 2 * random_speed()
+    if (y_2 <= r_2 and vy2 < 0) or (y_2 + r_2 >= MAX_Y and vy2 > 0):
+        vy2 = 2 * reflection(vy2)
+        vx2 = 2 * random_speed()
 
     move_target_0()
     move_target_1()
     move_special_target()
+
     score_text = FONT.render('Your score:' + str(score), True, (0, 255, 0))
     screen.blit(score_text, (10, 50))
+
     pygame.display.update()
     screen.fill(BLACK)
 
 pygame.quit()
 
-# best_players()
+best_players()
