6a92d3e9e54dbb3cf77f8d951ffade17c5c8aaef
alexgaborak <alexgaborak@phystech.edu>
2022-12-20 14:02:42 +0300

    Конфиг отдельным файлом

--- Pygame 2022/Catch the Balls 2022/catchme_main.py
+++ Pygame 2022/Catch the Balls 2022/catchme_main.py
@@ -1,28 +1,17 @@
 import pygame
 from pygame.draw import *
 from random import randint
+from catchme_config import *
 
 pygame.init()
 
-FPS = 2
+
 screen = pygame.display.set_mode((800, 800))
-
-# Цвета
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
-x = 0  # чтобы не вылетала ошибка при клике до появления первого шарика
-y = 0
-r = 0
 
 
 def new_ball():
+    # Надо бы, по-хорошему, класс для этого сделать
+    # сделаю, если останется время
     """
     Рисует "шарики" - круги случайного радиуса r
     (x, y) - случайные координаты центра кругов
@@ -40,7 +29,7 @@ def click(our_event):
     """
     Пока что эта функция выводит нам в консоль координаты и радиус шарика
     :param our_event: pygame event
-    :return: (x, y), r
+    :return (x, y), r:
     """
     print(x, y, r)
 
