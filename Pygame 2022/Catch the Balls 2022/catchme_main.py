6a1612b5e55b1fe703988c86da4675e67a354895
alexgaborak <alexgaborak@phystech.edu>
2022-12-20 16:54:14 +0300

    Подсчёт очков работает =)

--- Pygame 2022/Catch the Balls 2022/catchme_main.py
+++ Pygame 2022/Catch the Balls 2022/catchme_main.py
@@ -6,37 +6,53 @@ from catchme_config import *
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 
+# Шрифт
+FONT = pygame.font.SysFont('couriernew', 30, bold=True)
+
+score = 0
 
 def new_ball():
     # Надо бы, по-хорошему, класс для этого сделать
     # сделаю, если останется время
     """
-    Рисует "шарики" - круги случайного радиуса r
+    Рисует "шарики" - круги случайного радиуса r_0
     (x_0, y_0) - случайные координаты центра кругов
     color - случайный цвет из COLORS
     """
-    global x_0, y_0, r
+    global x_0, y_0, r_0
     x_0 = randint(100, 700)
     y_0 = randint(100, 700)
-    r = randint(50, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x_0, y_0), r)
+    r_0 = randint(50, 100)
+    color_0 = COLORS[randint(0, 5)]
+    circle(screen, color_0, (x_0, y_0), r_0)
 
 
 def other_ball():  # idk понадобится или нет
     """
-
-    :return:
+    Рисует "шарики" - круги случайного радиуса r_1
+    (x_1, y_1) - случайные координаты центра кругов
+    color - случайный цвет из COLORS
     """
-    pass
+    global x_1, y_1, r_1
+    x_1 = randint(100, 700)
+    y_1 = randint(100, 700)
+    r_1 = randint(50, 100)
+    color_1 = COLORS[randint(0, 5)]
+    circle(screen, color_1, (x_1, y_1), r_1)
 
 
-def third_figure():  # idk понадобится или нет (мб спец тим мишеней тоже круглыми сделаю)
+def third_figure():  # пока третий шарик
     """
-
-    :return:
+    Рисует "шарики" - круги случайного радиуса r_1
+    (x_1, y_1) - случайные координаты центра кругов
+    color - случайный цвет из COLORS
     """
-    pass
+    global x_2, y_2, r_2
+    x_2 = randint(100, 700)
+    y_2 = randint(100, 700)
+    r_2 = randint(50, 100)
+    color_2 = COLORS[randint(0, 5)]
+    circle(screen, color_2, (x_2, y_2), r_2)
 
 
 def check_click():
@@ -45,11 +61,15 @@ def check_click():
     :param : pygame event
     :return Bool:
     """
-    print(x_0, y_0, r)
+
     coord = pygame.mouse.get_pos()
     x = coord[0]
     y = coord[1]
-    return (x - x_0)**2 + (y - y_0)**2 <= r**2
+    hit0 = (x - x_0) ** 2 + (y - y_0) ** 2 <= r_0 ** 2
+    hit1 = (x - x_1) ** 2 + (y - y_1) ** 2 <= r_1 ** 2
+    #hit2 = (x - x_2) ** 2 + (y - y_2) ** 2 <= r_2 ** 2
+
+    return hit0 or hit1 #or hit2
 
 
 def move():
@@ -68,13 +88,14 @@ def reflection():
     pass
 
 
-def score():
+def current_score():
     """
-    Счёт
-    :return :
+    Выводит на экран текущий счёт
     """
-    pass
-
+    global score
+    score += check_click()
+    score_text = FONT.render('Your score:' + str(score), True, (0, 255, 0))
+    screen.blit(score_text, (10, 50))
 
 def best_players():
     """
@@ -97,11 +118,12 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-            zzz = check_click()
-            print(f" ", zzz)
+            check_click()
+            print(f'', check_click())
+    current_score()
+
     new_ball()
-    new_ball()  # Пока что для этого не срабатывает check_click
+    other_ball()
     pygame.display.update()
     screen.fill(BLACK)
 
